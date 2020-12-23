1ee867473389b10e93d83bbbb0d991470b61075d
mike <michaelwilsonsj@hotmail.com>
2020-12-23 15:44:13 -0500

    setup firebase realtime display

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import React from "react";
+import React, {useRef, useState } from "react";
 import "./App.css";
 
 import firebase from "firebase/app";
 import "firebase/firestore";
 import "firebase/auth";
+import 'firebase/analytics'; 
+
 import { useAuthState } from 'react-firebase-hooks/auth'; 
 import { useCollectionData } from 'react-firebase-hooks/firestore';
 
@@ -19,15 +21,62 @@ firebase.initializeApp({
 
 const auth = firebase.auth();
 const firestore = firebase.firestore(); 
+const analytics = firebase.analytics(); 
+
+
 
 function App() {
+  const [user] = useAuthState(auth); 
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Boilerplate</h1>
+      <header>
+        <h1>⚛️🔥💬</h1>
+        <SignOut />
       </header>
+
+      <section>
+        {user ? <ChatRoom /> : <SignIn />}
+      </section>
     </div>
   );
+}
+
+function SignIn() {
+  const signInWithGoogle = () => {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    auth.signInWithPopup(provider); 
+  }
+  return (
+    <button onClick={signInWithGoogle}>Sign in with Google</button>
+  )
+}
+
+function SignOut() {
+  return auth.currentUser && (
+    <button onClick={() => auth.signOut()}>Sign Out</button>
+  )
+}
+
+function ChatRoom() {
+  const dummy = useRef(); 
+  const messagesRef = firestore.collection('messages');
+  const query = messagesRef.orderBy('createdAt').limit(25); 
+  const [messages] = useCollectionData(query, {idField: 'id'}); 
+  return (
+    <>
+      <div>
+        {messages && messages.map(msg => <ChatMessage key={msg.id} message={msg}/>)}
+        <span ref={dummy}></span>
+      </div>
+    </>
+  )
+}
+
+function ChatMessage(props) {
+  const { text, uid } = props.message;
+  return (<>
+  <p>{text}</p>
+  </>)
 }
 
 export default App;
