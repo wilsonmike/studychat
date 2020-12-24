331d8315e4c9a2aec49e9feca97e9cfbccd3c6bb
mike <michaelwilsonsj@hotmail.com>
2020-12-23 21:45:52 -0500

    setup routing started questions page

--- src/App.js
+++ src/App.js
@@ -1,81 +1,179 @@
-import React, {useRef, useState } from "react";
-import "./App.css";
+import React, { useRef, useState } from 'react';
+import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Redirect
+} from "react-router-dom";
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+import 'firebase/analytics';
 
-import firebase from "firebase/app";
-import "firebase/firestore";
-import "firebase/auth";
-import 'firebase/analytics'; 
-
-import { useAuthState } from 'react-firebase-hooks/auth'; 
+import { useAuthState } from 'react-firebase-hooks/auth';
 import { useCollectionData } from 'react-firebase-hooks/firestore';
 
-firebase.initializeApp({
-  apiKey: "AIzaSyDKL27IeoKoOBX7Bs2ggNg2TAK8Rexxaj4",
+if (!firebase.apps.length) {
+  firebase.initializeApp({
+    apiKey: "AIzaSyDKL27IeoKoOBX7Bs2ggNg2TAK8Rexxaj4",
     authDomain: "studychat-16364.firebaseapp.com",
     projectId: "studychat-16364",
     storageBucket: "studychat-16364.appspot.com",
     messagingSenderId: "26638819191",
     appId: "1:26638819191:web:272ff5e58586773ef54b72",
     measurementId: "G-WTK3FYQ5XF"
-})
+  });
+}else {
+  firebase.app(); // if already initialized, use that one
+}
+
 
 const auth = firebase.auth();
-const firestore = firebase.firestore(); 
-const analytics = firebase.analytics(); 
-
+const firestore = firebase.firestore();
+const analytics = firebase.analytics();
 
 
 function App() {
-  const [user] = useAuthState(auth); 
-  return (
-    <div className="App">
-      <header>
-        <h1>⚛️🔥💬</h1>
-        <SignOut />
-      </header>
 
-      <section>
-        {user ? <ChatRoom /> : <SignIn />}
-      </section>
-    </div>
+  const [user] = useAuthState(auth);
+
+  return (
+    <Router>
+      <nav className="main-nav">
+          <ul>
+            <li>
+              <Link className="nav-item" to="/chat">Chat</Link>
+            </li>
+            <li>
+              <Link className="nav-item" to="/questions">Questions</Link>
+            </li>
+          </ul>
+        </nav>
+      <Switch>
+      <Route
+                exact
+                path="/"
+                render={() => {
+                    return (
+                      <Redirect to="/chat" /> 
+                    )
+                }}
+              />
+        <Route path="/chat">
+        <div className="App">
+          <header>
+            <h1>⚛️🔥💬</h1>
+            <nav className="main-nav">
+          <ul>
+            <li>
+              <Link className="nav-item" to="/questions">Questions</Link>
+            </li>
+          </ul>
+        </nav>
+            <SignOut />
+          </header>
+          <section>
+            {user ? <ChatRoom /> : <SignIn />}
+          </section>
+        </div>
+        </Route>
+        <Route path="/questions">
+          <Questions />
+        </Route>
+      </Switch>
+    </Router>
+    
   );
 }
 
 function SignIn() {
+
   const signInWithGoogle = () => {
     const provider = new firebase.auth.GoogleAuthProvider();
-    auth.signInWithPopup(provider); 
+    auth.signInWithPopup(provider);
   }
+
   return (
-    <button onClick={signInWithGoogle}>Sign in with Google</button>
+    <>
+      <button className="sign-in" onClick={signInWithGoogle}>Sign in with Google</button>
+    </>
   )
+
 }
 
 function SignOut() {
   return auth.currentUser && (
-    <button onClick={() => auth.signOut()}>Sign Out</button>
+    <button className="sign-out" onClick={() => auth.signOut()}>Sign Out</button>
   )
 }
+
 
 function ChatRoom() {
-  const dummy = useRef(); 
+  const dummy = useRef();
   const messagesRef = firestore.collection('messages');
-  const query = messagesRef.orderBy('createdAt').limit(25); 
-  const [messages] = useCollectionData(query, {idField: 'id'}); 
-  return (
-    <>
-      <div>
-        {messages && messages.map(msg => <ChatMessage key={msg.id} message={msg}/>)}
-        <span ref={dummy}></span>
-      </div>
-    </>
-  )
+  const query = messagesRef.orderBy('createdAt').limit(25);
+
+  const [messages] = useCollectionData(query, { idField: 'id' });
+
+  const [formValue, setFormValue] = useState('');
+
+
+  const sendMessage = async (e) => {
+    e.preventDefault();
+
+    const { uid, photoURL } = auth.currentUser;
+
+    await messagesRef.add({
+      text: formValue,
+      createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+      uid,
+      photoURL
+    })
+
+    setFormValue('');
+    dummy.current.scrollIntoView({ behavior: 'smooth' });
+  }
+
+  return (<>
+    <main>
+
+      {messages && messages.map(msg => <ChatMessage key={msg.id} message={msg} />)}
+
+      <span ref={dummy}></span>
+
+    </main>
+
+    <form onSubmit={sendMessage}>
+
+      <input value={formValue} onChange={(e) => setFormValue(e.target.value)} placeholder="say something nice" />
+
+      <button type="submit" disabled={!formValue}>🕊️</button>
+
+    </form>
+  </>)
 }
 
+
 function ChatMessage(props) {
-  const { text, uid } = props.message;
+  const { text, uid, photoURL } = props.message;
+
+  const messageClass = uid === auth.currentUser.uid ? 'sent' : 'received';
+
   return (<>
-  <p>{text}</p>
+    <div className={`message ${messageClass}`}>
+      <img src={photoURL || 'https://api.adorable.io/avatars/23/abott@adorable.png'} />
+      <p>{text}</p>
+    </div>
+  </>)
+}
+
+function Questions() {
+  return (<>
+    <div>
+      <h1>Questions</h1>
+    </div>
   </>)
 }
 
